99572054f989614449371c2947b8498b96d07090
Gabriel Rębacz <gabriel.rebacz@enigma.com.pl>
2020-12-11 21:06:59 +0100

    fix

--- src/data-analysis.r
+++ src/data-analysis.r
@@ -3,7 +3,7 @@ library(pacman)
 options(vsc.plot = "Two")
 pacman::p_load(skimr, dplyr, ggplot2, scales, corrplot, ROSE)
 dev.off()
-def_par = par()
+def_par = par(no.readonly = TRUE)
 
 print("Reading csv")
 data_df <- read.csv("creditcard.csv")
