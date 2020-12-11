0ee9997b88aa99448916c4f5cdb75fb35f42a887
Gabriel Rębacz <gabriel.rebacz@enigma.com.pl>
2020-12-11 21:04:52 +0100

    some refactor + more plots

--- src/data-analysis.r
+++ src/data-analysis.r
@@ -1,23 +1,19 @@
-# install.packages("skimr")
-# install.packages("corrplot")
-# install.packages("ROSE")
-library(skimr)
-library(dplyr)
-library(ggplot2)
-library(scales)
-library(corrplot)
-library(ROSE)
+#install.packages('pacman')
+library(pacman)
+options(vsc.plot = "Two")
+pacman::p_load(skimr, dplyr, ggplot2, scales, corrplot, ROSE)
+dev.off()
+def_par = par()
 
 print("Reading csv")
-ccfd_dframe <- read.csv("creditcard.csv")
+data_df <- read.csv("creditcard.csv")
 print("Basic information about dataset:")
 print("Head:")
-head(ccfd_dframe)
+head(data_df)
 print("Summary info of columns:")
-skim(ccfd_dframe)
+skim(data_df)
 
-
-nans <- sum(is.na(ccfd_dframe))
+nans <- sum(is.na(data_df))
 
 if (nans > 0) {
   sprintf("Dataset contains %d NaN values", nans)
@@ -26,17 +22,17 @@ if (nans > 0) {
   print("Dataset does not contain any NaN values")
 }
 
-ccfd_dframe$Class <- as.factor(ccfd_dframe$Class)
+data_df$Class <- as.factor(data_df$Class)
 
-ggplot(ccfd_dframe, aes(x = Class)) +
+ggplot(data_df, aes(x = Class)) +
   geom_bar(aes(y = (..count..) / sum(..count..))) +
   scale_y_continuous(labels = percent) +
   labs(y = "Percentage", x = "Class") +
   ggtitle("Percentage of classes in a dataset")
 
-hist(ccfd_dframe$Amount,
+hist(data_df$Amount,
   breaks = 500,
-  xlim = c(0, max(ccfd_dframe$Amount)),
+  xlim = c(0, max(data_df$Amount)),
   xlab = "Amount",
   ylab = "Frequency",
   main = "Histogram of amount of transactions"
@@ -44,51 +40,32 @@ hist(ccfd_dframe$Amount,
 
 
 # scale Amount and Time
-ccfd_dframe <- ccfd_dframe %>% mutate_at(c("Amount", "Time"), ~ (scale(.) %>% as.vector()))
+data_df <- data_df %>% mutate_at(c("Amount", "Time"), ~ (scale(.) %>% as.vector()))
+n_columns <- ncol(data_df)
+
+data_df$Class <- as.numeric(data_df$Class) - 1
+#correlation matrix for unbalanced set
+cor_matrix <- cor(data_df)
+corrplot(cor_matrix,
+         type = "lower",
+         tl.col = "black", tl.srt = 45,
+         main = "Correlation matrix for unbalanced set"
+)
 
 # Perform undersampling and plot correlation matrix
 
-ccfd_dframe_sub <- ovun.sample(Class ~ ., data = ccfd_dframe, method = "under")[["data"]]
-table(ccfd_dframe_sub$Class)
+undersampled_df <- ovun.sample(Class ~ ., data = data_df, method = "under")[["data"]]
+table(undersampled_df$Class)
 
-skim(ccfd_dframe_sub)
+skim(undersampled_df)
 
-ccfd_dframe_sub$Class <- as.numeric(ccfd_dframe_sub$Class)
+undersampled_df$Class <- as.numeric(undersampled_df$Class) - 1
 
-cor_matrix <- cor(ccfd_dframe_sub)
+cor_matrix <- cor(undersampled_df)
 corrplot(cor_matrix,
   type = "lower",
-  tl.col = "black", tl.srt = 45
-)
-
-
-n_columns <- ncol(ccfd_dframe)
-
-# Perform oversampling and plot correlation matrix
-# The parameters perc.over and perc.under control the amount of over-sampling of the minority class and under-sampling of the majority classes, respectively.
-# See also https://www.rdocumentation.org/packages/unbalanced/versions/2.0/topics/ubSMOTE
-
-ccfd_dframe_over <- ovun.sample(Class ~ ., data = ccfd_dframe, method = "over")[["data"]]
-
-table(ccfd_dframe$Class)
-table(ccfd_dframe_over$Class)
-
-
-skim(ccfd_dframe_over)
-
-ggplot(ccfd_dframe_over, aes(x = Class)) +
-  geom_bar(aes(y = (..count..) / sum(..count..))) +
-  scale_y_continuous(labels = percent) +
-  labs(y = "Percentage", x = "Class") +
-  ggtitle("Percentage of classes in an oversampled dataset")
-
-ccfd_dframe_over_numeric_class <- ccfd_dframe_over
-ccfd_dframe_over_numeric_class$Class <- as.numeric(ccfd_dframe_over_numeric_class$Class) - 1
-
-cor_matrix <- cor(ccfd_dframe_over_numeric_class)
-corrplot(cor_matrix,
-  type = "lower",
-  tl.col = "black", tl.srt = 45
+  tl.col = "black", tl.srt = 45,
+  main = "Correlation matrix for undersampled set"
 )
 
 # Take variables which have significant correlation with Class. Use oversampled set.
@@ -102,14 +79,65 @@ par(mar = c(7, 5, 1, 1))
 par(cex.lab = 1.5)
 
 for (big_corrs in c(big_negative_corrs, big_positive_corrs)) {
-  boxplot(ccfd_dframe_over[ccfd_dframe_over$Class == 0, ][, big_corrs],
-    ccfd_dframe_over[ccfd_dframe_over$Class == 1, ][, big_corrs],
+  boxplot(undersampled_df[undersampled_df$Class == 0, ][, big_corrs],
+    undersampled_df[undersampled_df$Class == 1, ][, big_corrs],
     names = c(0, 1),
     xlab = "Class",
     ylab = "Value",
     main = big_corrs
   )
 }
+
+par(def_par)
+
+# Perform oversampling and plot correlation matrix
+# The parameters perc.over and perc.under control the amount of over-sampling of the minority class and under-sampling of the majority classes, respectively.
+# See also https://www.rdocumentation.org/packages/unbalanced/versions/2.0/topics/ubSMOTE
+
+oversampled_df <- ovun.sample(Class ~ ., data = data_df, method = "over")[["data"]]
+
+table(data_df$Class)
+table(oversampled_df$Class)
+
+
+skim(oversampled_df)
+
+ggplot(oversampled_df, aes(x = Class)) +
+  geom_bar(aes(y = (..count..) / sum(..count..))) +
+  scale_y_continuous(labels = percent) +
+  labs(y = "Percentage", x = "Class") +
+  ggtitle("Percentage of classes in an oversampled dataset")
+
+oversampled_numveric_class_df <- oversampled_df
+oversampled_numveric_class_df$Class <- as.numeric(oversampled_numveric_class_df$Class) - 1
+
+cor_matrix <- cor(oversampled_numveric_class_df)
+corrplot(cor_matrix,
+  type = "lower",
+  tl.col = "black", tl.srt = 45,
+  main = "Correlation matrix for oversampled set"
+)
+
+# Take variables which have significant correlation with Class. Use oversampled set.
+
+class_corrs <- cor_matrix["Class", ][-n_columns]
+big_positive_corrs <- names(class_corrs)[class_corrs > 0.5]
+big_negative_corrs <- names(class_corrs)[class_corrs < -0.5]
+
+par(mfrow = c(2, floor((length(big_positive_corrs) + length(big_negative_corrs) / 2))))
+par(mar = c(7, 5, 1, 1))
+par(cex.lab = 1.5)
+
+for (big_corrs in c(big_negative_corrs, big_positive_corrs)) {
+  boxplot(oversampled_df[oversampled_df$Class == 0, ][, big_corrs],
+    oversampled_df[oversampled_df$Class == 1, ][, big_corrs],
+    names = c(0, 1),
+    xlab = "Class",
+    ylab = "Value",
+    main = big_corrs
+  )
+}
+par(def_par)
 
 # small_corrs <- names(class_corrs)[class_corrs < 0.1 & class_corrs > -0.1]
 # small_corrs
