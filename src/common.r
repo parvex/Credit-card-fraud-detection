505281e5dcf46b7575cdd70a3e04541ffb38eff0
MBelniak <rubik40s@wp.pl>
2021-01-20 00:27:53 +0100

    Create concept for saving training results and use them in markdown

---
+++ src/common.r
@@ -0,0 +1,88 @@
+checkNans <- function(data) {
+  nans <- sum(is.na(data))
+  
+  if (nans > 0) {
+    sprintf("Zbiór danych ma %d wartości NaN", nans)
+    # TODO eliminate NaNs in this case
+  } else {
+    print("Zbiór nie ma wartości NaN")
+  }
+}
+
+corMatrix <- function (data, title) {
+  data$Class <- as.numeric(data$Class) - 1
+  cor_matrix <- cor(data)
+  corrplot(cor_matrix,
+           type = "lower",
+           tl.col = "black", tl.srt = 45,
+           main = title,
+           mar=c(0,0,1,0)
+  )
+  return(cor_matrix)
+}
+
+create_log_tune_spec <- function () {
+  return(logistic_reg(penalty = tune(), mixture = 1) %>%
+          set_engine("glmnet") %>%
+          set_mode("classification"))
+}
+
+create_tree_tune_spec <- function () {
+  return(decision_tree(
+          cost_complexity = tune(),
+          tree_depth = tune(),
+          min_n = tune()
+         ) %>% 
+          set_engine("rpart") %>% 
+          set_mode("classification")
+        )
+}
+create_neural_net_spec <- function (epochs=20) {
+  return(mlp(epochs = epochs, 
+               hidden_units = 64)%>%
+             set_mode("classification") %>% 
+             set_engine("keras", verbose = 1)
+         # uses Adam optimizer by default
+         )
+}
+
+tune_with_data <- function (tune_spec, data, recipe, kfolds = 10) {
+  folds <- vfold_cv(data, v=kfolds, strata = Class)  
+  
+  tune_workflow <- workflow() %>% add_model(tune_spec) %>% add_recipe(recipe)
+  
+  HP_set <- parameters(tune_workflow)
+    
+  result <- tune_workflow %>% 
+    tune_bayes(
+      resamples = folds,
+      # To use non-default parameter ranges
+      param_info = HP_set,
+      # Generate five at semi-random to start
+      initial = 5,
+      iter = 10,
+      # How to measure performance?
+      metrics = metric_set(roc_auc),
+      control = control_bayes(no_improve = 5, verbose = TRUE))
+      
+    
+    best <- result %>% select_best("roc_auc")
+    final_workflow <- tune_workflow %>% finalize_workflow(best)
+  
+  return(final_workflow)
+}
+
+fit_and_eval <- function(workflow, data_split) {
+  final_fit <- last_fit(workflow, split = data_split)
+  
+  final_fit %>%
+    collect_metrics()
+
+  final_fit %>%
+    collect_predictions() %>% 
+    roc_curve(Class, .pred_0) %>% 
+    autoplot()
+  
+  return(final_fit)
+}
+
---
+++ src/models.r
@@ -0,0 +1,160 @@
+Sys.setlocale(category = "LC_ALL", locale = "Polish")
+install.packages("pacman")
+library(pacman)
+pacman::p_load(dplyr, 
+               scales, 
+               tidymodels, 
+               tune, 
+               workflows, 
+               tictoc, 
+               themis, 
+               reshape2, 
+               glmnet, 
+               tensorflow, 
+               keras,
+               mlbench,
+               caret,
+               e1071,
+               randomForest,
+               ranger)
+def_par = par(no.readonly = TRUE)
+#data loading
+if (!grepl('src$', getwd())) {
+  setwd("src")
+}
+source("common.r")
+load("rdata/datadf.Rdata")
+
+data_df <- data_df %>% mutate_at(c("Amount", "Time"), ~ (scale(.) %>% as.vector()))
+
+undersample_recipe <- recipe(Class~., data = data_df) %>%
+  themis::step_downsample(Class)
+
+undersampled_df <-undersample_recipe %>% prep(training = data_df) %>% bake(new_data=NULL)
+
+oversample_recipe <- recipe(Class~., data = data_df) %>%
+  themis::step_smote(Class)
+
+oversampled_df <- oversample_recipe %>% prep(training = data_df) %>% bake(new_data=NULL)
+
+# feature selection
+set.seed(7)
+trControl = trainControl(method="cv", number = 5, allowParallel = TRUE, verbose = TRUE)
+rf_fit <- train(Class ~ ., data = data_df, method = "ranger", num.trees = 50, trControl = trControl, importance = 'impurity')
+
+save(rf_fit, "rdata/rf_fit.Rdata")
+
+imp <- varImp(rf_fit, scale = FALSE)
+imp <- data.frame(imp[1])
+features <- rownames(imp)[order(imp$Overall, decreasing=TRUE)][1:10]
+
+data_df_selected <- data_df[,which(names(data_df) %in% c(features, "Class"))]
+
+selected_undersample_recipe <- recipe(Class~., data = data_df_selected) %>%
+  themis::step_downsample(Class) %>%
+  prep()
+
+selected_undersampled_df <- bake(selected_undersample_recipe, new_data=NULL)
+
+selected_oversample_recipe <- recipe(Class~., data = data_df_selected) %>%
+  themis::step_smote(Class) %>%
+  prep()
+
+selected_oversampled_df <- bake(selected_oversample_recipe, new_data=NULL)
+
+# splitting data into train and test sets
+data_split <- initial_split(data_df, strata = Class)
+data_train <- training(data_split)
+data_test <- testing(data_split)
+
+selected_data_split <- initial_split(data_df_selected, strata = Class)
+selected_data_train <- training(selected_data_split)
+selected_data_test <- testing(selected_data_split)
+
+log_reg_tune_spec <- create_log_tune_spec()
+tree_tune_spec <- create_tree_tune_spec()
+undersample_neural_net_spec <- create_neural_net_spec(epochs = 200)
+oversample_neural_net_spec <- create_neural_net_spec(epochs = 40)
+
+# LOGISTIC REGRESSION
+# training and testing on dataset with all features and using undersample
+log_reg_undersample_workflow <- tune_with_data(log_reg_tune_spec, data_train, undersample_recipe)
+log_req_undersample_final_fit <- fit_and_eval(log_reg_undersample_workflow, data_split)
+log_req_undersample_final_fit %>% collect_metrics()
+
+save(log_req_undersample_final_fit, file = "rdata/log_req_undersample_final_fit.Rdata")
+
+# training and testing on dataset with selected features and using undersample
+log_reg_undersample_selected_workflow <- tune_with_data(log_reg_tune_spec, selected_data_train, selected_undersample_recipe)
+log_req_undersample_selected_final_fit <- fit_and_eval(log_reg_undersample_selected_workflow, selected_data_split)
+log_req_undersample_selected_final_fit %>% collect_metrics()
+
+save(log_req_undersample_selected_final_fit, file = "rdata/log_req_undersample_selected_final_fit.Rdata")
+
+# training and testing on dataset with all features and using oversample
+log_reg_oversample_workflow <- tune_with_data(log_reg_tune_spec, data_train, oversample_recipe)
+log_req_oversample_final_fit <- fit_and_eval(log_reg_oversample_workflow, data_split)
+log_req_oversample_final_fit %>% collect_metrics()
+
+save(log_req_oversample_final_fit, file = "rdata/log_req_oversample_final_fit.Rdata")
+
+# r training and testing on dataset with selected features and using oversample
+log_reg_oversample_selected_workflow <- tune_with_data(log_reg_tune_spec, selected_data_train, selected_oversample_recipe)
+log_req_oversample_selected_final_fit <- fit_and_eval(log_reg_oversample_selected_workflow, selected_data_split)
+log_req_oversample_selected_final_fit %>% collect_metrics()
+
+save(log_req_oversample_selected_final_fit, file = "data/log_req_oversample_selected_final_fit.Rdata")
+
+
+# DECISION TREE
+
+tree_undersample_workflow <- tune_with_data(tree_tune_spec, data_train, undersample_recipe)
+tree_undersample_final_fit <- fit_and_eval(tree_undersample_workflow, data_split)
+tree_undersample_final_fit %>% collect_metrics()
+
+save(tree_undersample_final_fit, file = "rdata/tree_undersample_final_fit.Rdata")
+
+tree_undersample_selected_workflow <- tune_with_data(tree_tune_spec, selected_data_train, selected_undersample_recipe)
+tree_undersample_selected_final_fit <- fit_and_eval(tree_undersample_selected_workflow, selected_data_split)
+tree_undersample_selected_final_fit %>% collect_metrics()
+
+save(tree_undersample_selected_final_fit, file = "rdata/tree_undersample_selected_final_fit.Rdata")
+
+tree_oversample_workflow <- tune_with_data(tree_tune_spec, data_train, oversample_recipe)
+tree_oversample_final_fit <- fit_and_eval(tree_oversample_workflow, data_split)
+tree_oversample_final_fit %>% collect_metrics()
+
+save(tree_oversample_final_fit, file = "rdata/tree_oversample_final_fit.Rdata")
+
+tree_oversample_selected_workflow <- tune_with_data(tree_tune_spec, selected_data_train, selected_oversample_recipe)
+tree_oversample_selected_final_fit <- fit_and_eval(tree_oversample_selected_workflow, selected_data_split)
+tree_oversample_selected_final_fit %>% collect_metrics()
+
+save(tree_oversample_selected_final_fit, file = "rdata/tree_oversample_selected_final_fit.Rdata")
+
+
+# NEURAL NETWORK
+
+neural_net_undersample_workflow <- workflow() %>% add_model(undersample_neural_net_spec) %>% add_recipe(undersample_recipe)
+neural_net_undersample_final_fit <- fit_and_eval(neural_net_undersample_workflow, data_split)
+neural_net_undersample_final_fit %>% collect_metrics()
+
+save(neural_net_undersample_final_fit, file = "rdata/neural_net_undersample_final_fit.Rdata")
+
+neural_net_undersample_selected_workflow <- workflow() %>% add_model(undersample_neural_net_spec) %>% add_recipe(selected_undersample_recipe)
+neural_net_undersample_selected_final_fit <- fit_and_eval(neural_net_undersample_selected_workflow, selected_data_split)
+neural_net_undersample_selected_final_fit %>% collect_metrics()
+
+save(neural_net_undersample_selected_final_fit, file = "rdata/neural_net_undersample_selected_final_fit.Rdata")
+
+neural_net_oversample_workflow <- workflow() %>% add_model(oversample_neural_net_spec) %>% add_recipe(oversample_recipe)
+neural_net_oversample_final_fit <- fit_and_eval(neural_net_oversample_workflow, data_split)
+neural_net_oversample_final_fit %>% collect_metrics()
+
+save(neural_net_oversample_final_fit, file = "rdata/neural_net_oversample_final_fit.Rdata")
+
+neural_net_oversample_selected_workflow <- workflow() %>% add_model(oversample_neural_net_spec) %>% add_recipe(selected_oversample_recipe)
+neural_net_oversample_selected_final_fit <- fit_and_eval(neural_net_oversample_selected_workflow, selected_data_split)
+neural_net_oversample_selected_final_fit %>% collect_metrics()
+
+save(neural_net_oversample_selected_final_fit, file = "rdata/neural_net_oversample_selected_final_fit.Rdata")
