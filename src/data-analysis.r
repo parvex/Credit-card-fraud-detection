0cd0e8c06ff88b591985bcb8114de2f6e326386a
MBelniak <rubik40s@wp.pl>
2021-01-19 21:30:50 +0100

    Create 4 separate tune&train configurations, fix some shit

--- src/data-analysis.r
+++ src/data-analysis.r
@@ -21,19 +21,8 @@ corMatrix <- function (data, title) {
   return(cor_matrix)
 }
 
-bigCorrsBoxPlot <- function (data, cor_matrix) {
-  class_corrs <- cor_matrix["Class", ][-ncol(data)]
-  big_corrs <- names(class_corrs)[abs(class_corrs) > 0.5]
-  
-  melted <- data %>% select(big_corrs, Class) %>% melt(id.var = "Class")
-  ggplot(melted, aes(x=variable, y=value)) + 
-    geom_boxplot(aes(fill=Class)) + 
-    facet_wrap( ~ variable, scales="free")
-  data$Class <- as.factor(data$Class)
-}
-
 create_log_tune_spec <- function () {
-  return(logistic_reg(penalty = 10, mixture = 0.1) %>%
+  return(logistic_reg(penalty = tune(), mixture = 1) %>%
           set_engine("glmnet") %>%
           set_mode("classification"))
 }
@@ -53,43 +42,34 @@ create_neural_net_spec <- function (epochs=20) {
                hidden_units = 64)%>%
              set_mode("classification") %>% 
              set_engine("keras", verbose = 1)
+         # uses Adam optimizer by default
          )
 }
 
-tune_with_data <- function (tune_spec, data, recipes) {
-  folds <- vfold_cv(data, strata = Class)  
+tune_with_data <- function (tune_spec, data, recipe, kfolds = 10) {
+  folds <- vfold_cv(data, v=kfolds, strata = Class)  
   
-  tune_workflows <- list()
+  tune_workflow <- workflow() %>% add_model(tune_spec) %>% add_recipe(recipe)
   
-  for (recipe in recipes) {
-      new_workflow <- workflow() %>% add_model(tune_spec) %>% add_recipe(recipe)
-      tune_workflows <- append(tune_workflows, list(new_workflow))
-  }
-  
-  final_workflows <- list()
-  i <- 1
-  for (workflow in tune_workflows) {
-    HP_set <- parameters(workflow)
+  HP_set <- parameters(tune_workflow)
     
-    result <- workflow %>% 
-      tune_bayes(
-        resamples = folds,
-        # To use non-default parameter ranges
-        param_info = HP_set,
-        # Generate five at semi-random to start
-        initial = 5,
-        iter = 10,
-        # How to measure performance?
-        metrics = metric_set(roc_auc),
-        control = control_bayes(no_improve = 5, verbose = TRUE)
-      )
+  result <- tune_workflow %>% 
+    tune_bayes(
+      resamples = folds,
+      # To use non-default parameter ranges
+      param_info = HP_set,
+      # Generate five at semi-random to start
+      initial = 5,
+      iter = 10,
+      # How to measure performance?
+      metrics = metric_set(roc_auc),
+      control = control_bayes(no_improve = 5, verbose = TRUE))
+      
     
     best <- result %>% select_best("roc_auc")
-    final_workflows[[i]] <- workflow %>% finalize_workflow(best)
-    i <- i + 1
-  }
+    final_workflow <- tune_workflow %>% finalize_workflow(best)
   
-  return(final_workflows)
+  return(final_workflow)
 }
 
 fit_and_eval <- function(workflow, data_split) {
@@ -100,7 +80,9 @@ fit_and_eval <- function(workflow, data_split) {
 
   final_fit %>%
     collect_predictions() %>% 
-    roc_curve(Class, .pred_1) %>% 
+    roc_curve(Class, .pred_0) %>% 
     autoplot()
+  
+  return(final_fit)
 }
 
